9938b9482feca53e232f138971dae37a23d212f2
Calhoun5 <johnhckuo@gmail.com>
2018-03-02 09:05:54 +0800

    refactoring code

---
+++ src/script/ImageEditor.js
@@ -0,0 +1,184 @@
+import Jcrop from "jquery-jcrop"
+
+var ImageEditor = {
+
+  context: null,
+  image: null,
+  canvas: null,
+  filter_edited: false,
+  init: (context, image, canvas) => {
+      ImageEditor.context = context;
+      ImageEditor.image = image;
+      ImageEditor.canvas = canvas;
+  },
+  Filter: {
+    style: {},
+    defaultValue : {
+      blur: 0,
+      brightness: 100,
+      contrast: 100,
+      grayscale: 0,
+      hue_rotate: 0,
+      invert: 0,
+      opacity: 100,
+      saturate: 100,
+      sepia: 0
+    },
+    init: () => {
+      document.getElementById("grayscale").value = ImageEditor.Filter.defaultValue.grayscale;
+      document.getElementById("blur").value = ImageEditor.Filter.defaultValue.blur;
+      document.getElementById("sepia").value = ImageEditor.Filter.defaultValue.sepia;
+      document.getElementById("opacity").value = ImageEditor.Filter.defaultValue.opacity;
+      document.getElementById("brightness").value = ImageEditor.Filter.defaultValue.brightness;
+      document.getElementById("contrast").value = ImageEditor.Filter.defaultValue.contrast;
+      document.getElementById("hue_rotate").value = ImageEditor.Filter.defaultValue.hue_rotate;
+      document.getElementById("invert").value = ImageEditor.Filter.defaultValue.invert;
+      document.getElementById("saturate").value = ImageEditor.Filter.defaultValue.saturate;
+    },
+    blur: (val) => {
+      ImageEditor.Filter.style.blur = "blur(" + val + "px)";
+      ImageEditor.Filter.draw();
+    },
+    grayscale: (val) => {
+      ImageEditor.Filter.style.grayscale = "grayscale(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    sepia: (val) => {
+      ImageEditor.Filter.style.sepia = "sepia(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    saturate: (val) => {
+      ImageEditor.Filter.style.saturate = "saturate(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    hue_rotate: (val) => {
+      ImageEditor.Filter.style.hue_rotate = "hue-rotate(" + val + "deg)";
+      ImageEditor.Filter.draw();
+    },
+    invert: (val) => {
+      ImageEditor.Filter.style.invert = "invert(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    opacity: (val) => {
+      ImageEditor.Filter.style.opacity = "opacity(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    brightness: (val) => {
+      ImageEditor.Filter.style.brightness = "brightness(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    contrast: (val) => {
+      ImageEditor.Filter.style.contrast = "contrast(" + val + "%)";
+      ImageEditor.Filter.draw();
+    },
+    reset: () => {
+      ImageEditor.Filter.style = {};
+      ImageEditor.context.filter = "none";
+      ImageEditor.Filter.draw();
+    },
+    draw: () => {
+      var sum = "";
+      for (var key in ImageEditor.Filter.style) {
+         if (ImageEditor.Filter.style.hasOwnProperty(key)) {
+            sum += ImageEditor.Filter.style[key] + " ";
+         }
+      }
+      ImageEditor.context.filter = sum;
+      ImageEditor.context.drawImage(ImageEditor.image, 0, 0);
+      ImageEditor.context.filter = "none";
+    }
+  },
+  Editor: {
+    crop_max_width: window.innerWidth * 0.5,
+    crop_max_height: window.innerHeight * 0.5,
+    jcrop_api: null,
+    prefsize: null,
+    selectcanvas: function(coords) {
+      ImageEditor.Editor.prefsize = {
+        x: Math.round(coords.x),
+        y: Math.round(coords.y),
+        w: Math.round(coords.w),
+        h: Math.round(coords.h)
+      };
+    },
+    applyCrop: function() {
+      ImageEditor.Editor.reapplyFilter();
+      ImageEditor.context.drawImage(ImageEditor.image, ImageEditor.Editor.prefsize.x, ImageEditor.Editor.prefsize.y, ImageEditor.Editor.prefsize.w, ImageEditor.Editor.prefsize.h, 0, 0, ImageEditor.canvas.width, ImageEditor.canvas.height);
+    },
+
+    applyScale: function(scale) {
+      if (scale == 1) return;
+      ImageEditor.canvas.width = ImageEditor.canvas.width * scale;
+      ImageEditor.canvas.height = ImageEditor.canvas.height * scale;
+      ImageEditor.Editor.reapplyFilter();
+      ImageEditor.context.drawImage(ImageEditor.image, 0, 0, ImageEditor.canvas.width, ImageEditor.canvas.height);
+    },
+
+    applyRotate: function() {
+      ImageEditor.canvas.width = ImageEditor.image.height;
+      ImageEditor.canvas.height = ImageEditor.image.width;
+      ImageEditor.context.clearRect(0, 0, ImageEditor.canvas.width, ImageEditor.canvas.height);
+      ImageEditor.context.translate(ImageEditor.image.height / 2, ImageEditor.image.width / 2);
+      ImageEditor.context.rotate(Math.PI / 2);
+      ImageEditor.Editor.reapplyFilter();
+      ImageEditor.context.drawImage(ImageEditor.image, -ImageEditor.image.width / 2, -ImageEditor.image.height / 2);
+    },
+
+    applyHflip: function() {
+      ImageEditor.context.clearRect(0, 0, ImageEditor.canvas.width, ImageEditor.canvas.height);
+      ImageEditor.context.translate(ImageEditor.image.width, 0);
+      ImageEditor.context.scale(-1, 1);
+      ImageEditor.Editor.reapplyFilter();
+      ImageEditor.context.drawImage(ImageEditor.image, 0, 0);
+    },
+
+    applyVflip: function() {
+      ImageEditor.context.clearRect(0, 0, ImageEditor.canvas.width, ImageEditor.canvas.height);
+      ImageEditor.context.translate(0, ImageEditor.image.height);
+      ImageEditor.context.scale(1, -1);
+      ImageEditor.Editor.reapplyFilter();
+      ImageEditor.context.drawImage(ImageEditor.image, 0, 0);
+    },
+    reapplyFilter: function(){
+
+      if (!ImageEditor.filter_edited){
+        return;
+      }
+
+      ImageEditor.filter_edited = false;
+      var sum = "";
+      for (var key in ImageEditor.Filter.style) {
+         if (ImageEditor.Filter.style.hasOwnProperty(key)) {
+            sum += ImageEditor.Filter.style[key] + " ";
+         }
+      }
+      ImageEditor.context.filter = sum;
+    },
+    jcropInit: function(canvasElem) {
+      canvasElem.Jcrop({
+        onSelect: ImageEditor.Editor.selectcanvas,
+        onRelease: ImageEditor.Editor.clearcanvas,
+        boxWidth: ImageEditor.Editor.crop_max_width,
+        boxHeight: ImageEditor.Editor.crop_max_height
+      }, function() {
+        ImageEditor.Editor.jcrop_api = this;
+      });
+      ImageEditor.Editor.clearcanvas();
+    },
+    clearcanvas: function() {
+      ImageEditor.Editor.prefsize = {
+        x: 0,
+        y: 0,
+        w: ImageEditor.canvas.width,
+        h: ImageEditor.canvas.height,
+      };
+    }
+  },
+
+
+
+
+}
+
+
+export default ImageEditor;
--- src/script/main.js
+++ src/script/main.js
@@ -1,21 +1,13 @@
 import '../scss/reset.scss';
 import '../scss/main.scss';
 
-import Jcrop from "jquery-jcrop"
 import swal from 'sweetalert2'
 import firebase from "firebase"
+import ImageEditor from "./ImageEditor"
 
-import Filter from "./Filter"
-
-var crop_max_width = window.innerWidth * 0.5;
-var crop_max_height = window.innerHeight * 0.5;
-var jcrop_api;
 var canvas;
 var context;
 var image, originalImage;
-var filter_edited = false;
-
-var prefsize;
 
 init();
 
@@ -97,9 +89,9 @@ document.getElementById("file").addEventListener("change", function(e){
 }, false);
 
 function recacheImage(){
-	if (!filter_edited){
+	if (!ImageEditor.filter_edited){
 		validateImage();
-		filter_edited = true;
+		ImageEditor.filter_edited = true;
 	}
 }
 
@@ -142,11 +134,11 @@ function dataURLtoBlob(dataURL) {
 }
 
 function validateImage() {
-  if (canvas != null) {
-    image = new Image();
-    image.onload = restartJcrop;
-    image.src = canvas.toDataURL();
-  } else restartJcrop();
+	if (canvas != null) {
+	  image = new Image();
+	  image.onload = restartJcrop;
+	  image.src = canvas.toDataURL();
+	} else restartJcrop();
 }
 
 function restartJcrop() {
@@ -159,32 +151,26 @@ function restartJcrop() {
 	}
 	var canvasElem = document.createElement("canvas");
 	canvasElem.id = "canvas";
-  views.appendChild(canvasElem);
-  canvas = canvasElem;
-  context = canvas.getContext("2d");
+	views.appendChild(canvasElem);
+	canvas = canvasElem;
+	context = canvas.getContext("2d");
 
-  canvas.width = image.width;
-  canvas.height = image.height;
-  context.drawImage(image, 0, 0);
+	canvas.width = image.width;
+	canvas.height = image.height;
+	context.drawImage(image, 0, 0);
 
-	if (jcrop_api != null) {
-		jcrop_api.destroy();
-		Filter.image = image;
-		Filter.context = context;
+	if (ImageEditor.Editor.jcrop_api != null) {
+	  ImageEditor.Editor.jcrop_api.destroy();
+	  ImageEditor.image = image;
+	  ImageEditor.context = context;
 	}else{
-		Filter.init(context, image);
-		originalImage = image;
+	  ImageEditor.init(context, image, canvas);
+	  ImageEditor.Filter.init();
+	  originalImage = image;
 	}
 
-  $("#canvas").Jcrop({
-    onSelect: selectcanvas,
-    onRelease: clearcanvas,
-    boxWidth: crop_max_width,
-    boxHeight: crop_max_height
-  }, function() {
-    jcrop_api = this;
-  });
-  clearcanvas();
+  	ImageEditor.Editor.jcropInit($("#canvas"));
+
 }
 
 function showAdvancePanel(){
@@ -192,157 +178,89 @@ function showAdvancePanel(){
 	$(".initial_panel").fadeOut();
 }
 
-function clearcanvas() {
-  prefsize = {
-    x: 0,
-    y: 0,
-    w: canvas.width,
-    h: canvas.height,
-  };
-}
 
-function selectcanvas(coords) {
-  prefsize = {
-    x: Math.round(coords.x),
-    y: Math.round(coords.y),
-    w: Math.round(coords.w),
-    h: Math.round(coords.h)
-  };
-}
 
-function applyCrop() {
-	reapplyFilter();
-  context.drawImage(image, prefsize.x, prefsize.y, prefsize.w, prefsize.h, 0, 0, canvas.width, canvas.height);
-  validateImage();
-}
 
-function applyScale(scale) {
-  if (scale == 1) return;
-  canvas.width = canvas.width * scale;
-  canvas.height = canvas.height * scale;
-	reapplyFilter();
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
-  validateImage();
-}
-
-function applyRotate() {
-	canvas.width = image.height;
-  canvas.height = image.width;
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.translate(image.height / 2, image.width / 2);
-  context.rotate(Math.PI / 2);
-	reapplyFilter();
-  context.drawImage(image, -image.width / 2, -image.height / 2);
-  validateImage();
-}
-
-function applyHflip() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-  context.translate(image.width, 0);
-  context.scale(-1, 1);
-	reapplyFilter();
-  context.drawImage(image, 0, 0);
-  validateImage();
-}
-
-function applyVflip() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.translate(0, image.height);
-  context.scale(1, -1);
-	reapplyFilter();
-  context.drawImage(image, 0, 0);
-  validateImage();
-}
-
-function reapplyFilter(){
-
-	if (!filter_edited){
-		return;
-	}
-
-	filter_edited = false;
-	var sum = "";
-	for (var key in Filter.style) {
-		 if (Filter.style.hasOwnProperty(key)) {
-				sum += Filter.style[key] + " ";
-		 }
-	}
-	context.filter = sum;
-}
 
 document.getElementById("crop").addEventListener("click", function(e){
-	applyCrop();
+	ImageEditor.Editor.applyCrop();
+	validateImage();
 }, false);
 
 document.getElementById("scale").addEventListener("click", function(e){
 	var scale = prompt("Scale Factor:", "1");
-	applyScale(scale);
+	ImageEditor.Editor.applyScale(scale);
+	validateImage();
 }, false);
 
 document.getElementById("rotate").addEventListener("click", function(e){
-	applyRotate();
+	ImageEditor.Editor.applyRotate();
+	validateImage();
 }, false);
 
 document.getElementById("hflip").addEventListener("click", function(e){
-	applyHflip();
+	ImageEditor.Editor.applyHflip();
+	validateImage();
 }, false);
 
 document.getElementById("vflip").addEventListener("click", function(e){
-	applyVflip();
+	ImageEditor.Editor.applyVflip();
+	validateImage();
 }, false);
 
 document.getElementById("blur").addEventListener("input", function(e){
 	recacheImage();
-	Filter.blur(e.target.value);
+	ImageEditor.Filter.blur(e.target.value);
 }, false);
 
 document.getElementById("grayscale").addEventListener("input", function(e){
 	recacheImage();
-	Filter.grayscale(e.target.value);
+	ImageEditor.Filter.grayscale(e.target.value);
 }, false);
 
 document.getElementById("sepia").addEventListener("input", function(e){
 	recacheImage();
-	Filter.sepia(e.target.value);
+	ImageEditor.Filter.sepia(e.target.value);
 }, false);
 
 document.getElementById("saturate").addEventListener("input", function(e){
 	recacheImage();
-	Filter.saturate(e.target.value);
+	ImageEditor.Filter.saturate(e.target.value);
 }, false);
 
 document.getElementById("hue_rotate").addEventListener("input", function(e){
 	recacheImage();
-	Filter.hue_rotate(e.target.value);
+	ImageEditor.Filter.hue_rotate(e.target.value);
 }, false);
 
 document.getElementById("invert").addEventListener("input", function(e){
 	recacheImage();
-	Filter.invert(e.target.value);
+	ImageEditor.Filter.invert(e.target.value);
 }, false);
 
 document.getElementById("opacity").addEventListener("input", function(e){
 	recacheImage();
-	Filter.opacity(e.target.value);
+	ImageEditor.Filter.opacity(e.target.value);
 }, false);
 
 document.getElementById("brightness").addEventListener("input", function(e){
 	recacheImage();
-	Filter.brightness(e.target.value);
+	ImageEditor.Filter.brightness(e.target.value);
 }, false);
 
 document.getElementById("contrast").addEventListener("input", function(e){
 	recacheImage();
-	Filter.contrast(e.target.value);
+	ImageEditor.Filter.contrast(e.target.value);
 }, false);
 
 document.getElementById("reset").addEventListener("click", function(e){
-	Filter.reset();
-	Filter.init(context, image);
-	jcrop_api = null;
+	ImageEditor.Filter.reset();
+	ImageEditor.init(context, image, canvas);
+	ImageEditor.Filter.init();
+	ImageEditor.Editor.jcrop_api = null;
 	image = originalImage;
 	restartJcrop();
-	filter_edited = false;
+	ImageEditor.filter_edited = false;
 }, false);
 
 document.getElementById("form").addEventListener("submit", function(e){
